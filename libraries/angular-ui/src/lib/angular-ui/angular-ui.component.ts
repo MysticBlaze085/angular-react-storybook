747dfeb4d77786bdfff64984d5ad253134c48ec8
MysticBlaze085 <seerblazesolutions@gmail.com>
2024-02-13 16:42:28 +0000

    chang environment build

---
+++ libraries/angular-ui/src/lib/angular-ui/angular-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rhapsody-angular-ui',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './angular-ui.component.html',
+  styleUrl: './angular-ui.component.css',
+})
+export class AngularUiComponent {}
