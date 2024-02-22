0f7ebba97d3e436fc37769fc4baea22968f82e88
MysticBlaze085 <seerblazesolutions@gmail.com>
2024-02-22 19:57:47 +0000

    folder structure change

--- shared-components/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/angular-components';
-
-export { default as MyReactComponent } from './lib/react-components/my-react-component/my-react-component';
--- shared-components/src/lib/angular-components/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './my-angular-component/my-angular-component.component'
