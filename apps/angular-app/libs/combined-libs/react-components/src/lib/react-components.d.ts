6992717422474f9a415574d248bab1b577e8f392
MysticBlaze085 <seerblazesolutions@gmail.com>
2024-02-25 00:17:34 +0000

    changes to try

---
+++ apps/angular-app/libs/combined-libs/react-components/src/lib/react-components.d.ts
@@ -0,0 +1,4 @@
+export interface ReactComponentsProps {
+}
+export declare function ReactComponents(props: ReactComponentsProps): import("react/jsx-runtime").JSX.Element;
+export default ReactComponents;
